5fc2c277974a646ca52d5736c2aea9b38a95e026
GaVender <lin1619@163.com>
2020-04-26 23:47:46 +0800

    feat(mongodb): 修改mongodb，添加prometheus

--- pkg/mongodb/mongodb.go
+++ pkg/mongodb/mongodb.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/opentracing/opentracing-go"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/event"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -17,10 +16,6 @@ import (
 )
 
 type (
-	Mongo struct {
-		*mongo.Client
-	}
-
 	Config struct {
 		App             string
 		Hosts           []string
@@ -29,10 +24,22 @@ type (
 		MinPoolSize     uint64
 	}
 
-	hook struct {
-		tracer opentracing.Tracer
-		log    log.Logger
+	Mongo struct {
+		*mongo.Client
+		logger          log.Logger
+		tracer          opentracing.Tracer
+		ableMonitor     bool
+		monitorInterval time.Duration
+		close           chan bool
 	}
+
+	hook struct {
+		tracer      opentracing.Tracer
+		logger      log.Logger
+		ableMonitor bool
+	}
+
+	Option func(*Mongo)
 )
 
 const (
@@ -44,11 +51,20 @@ var (
 	startEvent sync.Map
 )
 
-func NewConnection(cfg Config, tracer opentracing.Tracer, log log.Logger) (Mongo, func()) {
+func NewConnection(cfg Config, opts ...Option) (Mongo, func()) {
+	m := Mongo{
+		close: make(chan bool),
+	}
+
+	for _, opt := range opts {
+		opt(&m)
+	}
+
 	idleTime := time.Duration(cfg.MaxConnIdleTime)
 	h := hook{
-		tracer: tracer,
-		log:    log,
+		tracer:      m.tracer,
+		logger:      m.logger,
+		ableMonitor: m.ableMonitor,
 	}
 
 	client, err := mongo.Connect(
@@ -65,6 +81,9 @@ func NewConnection(cfg Config, tracer opentracing.Tracer, log log.Logger) (Mongo
 			Succeeded: h.succeed(),
 			Failed:    h.fail(),
 		}),
+		options.Client().SetPoolMonitor(&event.PoolMonitor{
+			Event: h.poolMonitor(),
+		}),
 	)
 	if err != nil {
 		panic("mongodb init: " + err.Error())
@@ -75,13 +94,32 @@ func NewConnection(cfg Config, tracer opentracing.Tracer, log log.Logger) (Mongo
 		panic("mongodb ping: " + err.Error())
 	}
 
-	return Mongo{
-			client,
-		}, func() {
-			if err = client.Disconnect(ctx); err != nil {
-				log.Errorf("mongodb close: %s", err.Error())
-			}
+	m.Client = client
+	return m, func() {
+		if err = client.Disconnect(ctx); err != nil {
+			close(m.close)
+			m.logger.Errorf("mongodb close: %s", err.Error())
 		}
+	}
+}
+
+func WithLogger(logger log.Logger) Option {
+	return func(m *Mongo) {
+		m.logger = logger
+	}
+}
+
+func WithTracer(tracer opentracing.Tracer) Option {
+	return func(m *Mongo) {
+		m.tracer = tracer
+	}
+}
+
+func WithMonitor(able bool, interval time.Duration) Option {
+	return func(m *Mongo) {
+		m.ableMonitor = able
+		m.monitorInterval = interval
+	}
 }
 
 func (h hook) start() func(context.Context, *event.CommandStartedEvent) {
@@ -101,27 +139,33 @@ func (h hook) succeed() func(context.Context, *event.CommandSucceededEvent) {
 		if !ok {
 			startedTime = time.Now()
 		}
-		startedEvent, ok := startEvent.Load(succeededEvent.RequestID)
-		command := bson.Raw{}
-		if ok {
-			if _, ok := startedEvent.(*event.CommandStartedEvent); ok {
-				command = startedEvent.(*event.CommandStartedEvent).Command
-			}
+		unknownEvent, ok1 := startEvent.Load(succeededEvent.RequestID)
+		startedEvent, ok2 := unknownEvent.(*event.CommandStartedEvent)
+		if !ok1 || !ok2 {
+			startedEvent = &event.CommandStartedEvent{}
 		}
 
 		operationInfo := operation + succeededEvent.CommandName
 
-		msp := h.tracer.StartSpan(
-			operationInfo,
-			opentracing.ChildOf(opentracing.SpanFromContext(ctx).Context()),
-			opentracing.StartTime(startedTime.(time.Time)),
-		).
-			SetTag("mongodb request id", succeededEvent.RequestID).
-			SetTag("command", command).
-			SetTag("result", succeededEvent.Reply.String())
-		msp.Finish()
+		if h.tracer != nil {
+			msp := h.tracer.StartSpan(
+				operationInfo,
+				opentracing.ChildOf(opentracing.SpanFromContext(ctx).Context()),
+				opentracing.StartTime(startedTime.(time.Time)),
+			).
+				SetTag("mongodb request id", succeededEvent.RequestID).
+				SetTag("command", startedEvent.Command).
+				SetTag("result", succeededEvent.Reply.String())
+			msp.Finish()
+		}
 
-		h.log.Infof(fmt.Sprint(operationInfo, " , duration: ", succeededEvent.DurationNanos/1e6))
+		if h.ableMonitor {
+			metricsMongodbQueryCounter.WithLabelValues(startedEvent.DatabaseName, succeededEvent.CommandName, "success").Inc()
+			metricsMongodbDurationHistogram.WithLabelValues(startedEvent.DatabaseName, startedEvent.Command.String()).
+				Observe(float64(succeededEvent.DurationNanos / 1e6))
+		}
+
+		h.logger.Infof(fmt.Sprint(operationInfo, " , duration: ", succeededEvent.DurationNanos/1e6))
 		startTime.Delete(succeededEvent.RequestID)
 		startEvent.Delete(succeededEvent.RequestID)
 	}
@@ -137,28 +181,42 @@ func (h hook) fail() func(context.Context, *event.CommandFailedEvent) {
 		if !ok {
 			startedTime = time.Now()
 		}
-		startedEvent, ok := startEvent.Load(failedEvent.RequestID)
-		command := bson.Raw{}
-		if ok {
-			if _, ok := startedEvent.(*event.CommandStartedEvent); ok {
-				command = startedEvent.(*event.CommandStartedEvent).Command
-			}
+		unknownEvent, ok1 := startEvent.Load(failedEvent.RequestID)
+		startedEvent, ok2 := unknownEvent.(*event.CommandStartedEvent)
+		if !ok1 || !ok2 {
+			startedEvent = &event.CommandStartedEvent{}
 		}
 
 		operationInfo := operation + failedEvent.CommandName
 
-		msp := h.tracer.StartSpan(
-			operationInfo,
-			opentracing.ChildOf(opentracing.SpanFromContext(ctx).Context()),
-			opentracing.StartTime(startedTime.(time.Time)),
-		).
-			SetTag("mongodb request id", failedEvent.RequestID).
-			SetTag("command", command).
-			SetTag("error", failedEvent.Failure)
-		msp.Finish()
+		if h.tracer != nil {
+			msp := h.tracer.StartSpan(
+				operationInfo,
+				opentracing.ChildOf(opentracing.SpanFromContext(ctx).Context()),
+				opentracing.StartTime(startedTime.(time.Time)),
+			).
+				SetTag("mongodb request id", failedEvent.RequestID).
+				SetTag("command", startedEvent.Command).
+				SetTag("error", failedEvent.Failure)
+			msp.Finish()
+		}
 
-		h.log.Errorf(fmt.Sprint(operationInfo, " , duration: ", failedEvent.DurationNanos/1e6))
+		if h.ableMonitor {
+			metricsMongodbQueryCounter.WithLabelValues(startedEvent.DatabaseName, failedEvent.CommandName, "fail").Inc()
+			metricsMongodbDurationHistogram.WithLabelValues(startedEvent.DatabaseName, startedEvent.Command.String()).
+				Observe(float64(failedEvent.DurationNanos / 1e6))
+		}
+
+		h.logger.Errorf(fmt.Sprint(operationInfo, " , duration: ", failedEvent.DurationNanos/1e6))
 		startTime.Delete(failedEvent.RequestID)
 		startEvent.Delete(failedEvent.RequestID)
+	}
+}
+
+func (h hook) poolMonitor() func(poolEvent *event.PoolEvent) {
+	return func(poolEvent *event.PoolEvent) {
+		if h.ableMonitor {
+			metricsMongodbStatsGauge.WithLabelValues(poolEvent.Address, poolEvent.Type).Inc()
+		}
 	}
 }
